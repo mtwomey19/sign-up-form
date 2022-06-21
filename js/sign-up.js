76923c15aa4796d860e960cb6df8d9707281e3ed
mtwomey19 <mtwomey19@gmail.com>
2022-06-20 20:12:33 -0400

    Color email border based on input

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,15 +1,26 @@
 
 function checkName(element) {
-    element.addEventListener('blur', (event) => {
-    if (element.value === '') {
-        element.style.border = "1px solid #D98880";
+    element.addEventListener('blur', () => {
+    if (element.value !== '') {
+        element.style.border = "1px solid #16A085";
     }
     else {
-        element.style.border = "1px solid #16A085"
+        element.style.border = "1px solid #D98880";
     };
     });
 }
-function checkEmail() {}
+function checkEmail(element) {
+    element.addEventListener('blur', () => {
+    if (element.value
+        .toLowerCase()
+        .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            element.style.border = "1px solid #16A085";
+        } else {
+            element.style.border = "1px solid #D98880";
+        }
+    });
+
+}
 
 function checkPhoneNum() {}
 
@@ -19,5 +30,8 @@ function comparePasswords() {}
 
 const fName = document.getElementById('f-name');
 const lName = document.getElementById('l-name');
+const email = document.getElementById('email')
+
 checkName(fName);
 checkName(lName);
+checkEmail(email);
